589bcad89751b24f4e1f5acb9c815978e6836166
baraah_adnan <baraahadnan@gmail.com>
2024-10-23 23:15:48 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//open close cart 
+var cart = document.querySelector('.cart');
+function open() {
+    cart.classList.add("active")
+
+}
+function close() {
+    cart.classList.remove("active")
+    
+}
